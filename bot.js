4993a80147253407102741e5ba892687933e357b
joeywoah23 <58614907+joeywoah23@users.noreply.github.com>
2020-03-17 15:20:58 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -835,8 +835,8 @@ return message.reply("You can\'t report yourself!");
         // args = ["Is", "this", "the", "real", "life?"]
         const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
         const command = args.shift().toLowerCase();
-        
-	        const Enmap = require("enmap");
+              });
+	const Enmap = require("enmap");
 client.points = new Enmap({name: "points"});
 client.on("message", message => {
   if (message.author.bot) return;
